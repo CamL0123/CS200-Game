6d1d8a89d8b78d9ab5b6c71e0f185fd2caaf906d
IsabelaSanchez <isabelanatyst26@gmail.com>
2025-04-17 12:55:01 -0400

    Adding snake

---
+++ Snake.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Snake here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+
+public class Snake extends Animal {
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Snake
+     */
+    public Snake()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+}
