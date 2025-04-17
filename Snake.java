95d2ad5a44937c26846ac1710a5ecaf00f25013c
IsabelaSanchez <isabelanatyst26@gmail.com>
2025-04-17 16:44:43 -0400

    snake v4

--- Snake.java
+++ Snake.java
@@ -15,33 +15,31 @@ public class Snake extends Animal {
         super("Snake", 10);
     }
     
-    public boolean SnakeRiddle(){
+    public static void main(String[]args){
     Scanner scanner = new Scanner(System.in);  
     String answerA;
     String answerB;
     String answerC;
-    answerA = "A king cobra"; 
-    answerB = "A sand snake.";
-    answerC = "A poisonous snake.";
+    answerA = "king cobra"; 
+    answerB = "sand snake";
+    answerC = "poisonous snake";
     // Riddle
-        System.out.println("Snake Challenge: Riddle! Answer the following question and you will be able to open the door to the next room");
-        System.out.println("Category is Snakes!!!");
-        System.out.println("With scales that shimmer like jewels, I am a creature of beauty and danger. What am I?");
-        System.out.println("A. " + answerA);
-        System.out.println("B. " + answerB);
-        System.out.println("C. " + answerC);
-        System.out.print("Enter your answer (e.g., A): ");
+    System.out.println("Snake Challenge: Riddle! Answer the following question and you will be able to open the door to the next room");
+    System.out.println("Category is Snakes!!!");
+    System.out.println("With scales that shimmer like jewels, I am a creature of beauty and danger. What am I?");
+    System.out.println("A " + answerA);
+    System.out.println("B " + answerB);
+    System.out.println("C " + answerC);
+    System.out.print("Enter your answer (e.g., A): ");
 
-        String answer = scanner.nextLine().toUpperCase();
+    String answer = scanner.nextLine().toUpperCase();
 
-        if (answer.equals(answerA)) {
-            System.out.println("Congratulations! You open the door to the next room.");
-        } else {
-            System.out.println("Wrong answer! Snake wants to bite you");
-            
-        }
-        return true;
+    if (answer.equals("A")) {
+        System.out.println("Congratulations! You open the door to the next room.");
+    } else {
+        System.out.println("Wrong answer! Snake wants to bite you");
     }
-
+        
+    }
 
 }
