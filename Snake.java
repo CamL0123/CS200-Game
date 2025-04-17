017eea27dd1e0dcc6e855be0eaca020a7c61461c
IsabelaSanchez <isabelanatyst26@gmail.com>
2025-04-17 15:00:06 -0400

    basic snake

--- Snake.java
+++ Snake.java
@@ -9,21 +9,9 @@ import java.util.*;
 
 public class Snake extends Animal {
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Snake
-     */
-    public Snake()
-    {
-        // initialise instance variables
-        x = 0;
+    // Constructor
+    public Snake() {
+        super("Snake");
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
 }
