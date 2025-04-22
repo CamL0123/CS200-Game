8085afd3bad85aed78e469fd07abec3fe75b4694
yy106-Elaine <yy106@wellesley.edu>
2025-04-22 01:10:36 -0400

    Update FactSet.java

--- FactSet.java
+++ FactSet.java
@@ -8,4 +8,14 @@ public class FactSet {
         this.C = C;
         this.truths = new String[]{truth1, truth2};
     }
+
+    public static List<FactSet> getOwlFactSets() {
+        List<FactSet> list = new ArrayList<>();
+        list.add(new FactSet("Owls can rotate their heads 270°.", "Owls are mammals.", "Owls hunt at night.", "A", "C"));
+        list.add(new FactSet("Owls have excellent vision.", "Owls can swim long distances.", "Owls regurgitate bones.", "A", "C"));
+        list.add(new FactSet("Owls lay eggs.", "Owls build nests underground.", "Owls have asymmetrical ears.", "A", "C"));
+        list.add(new FactSet("The Great Wall of China is visible from space.", "Water boils at a lower temperature at higher altitudes.", "Bananas are berries, but strawberries are not.", "B", "C"));
+        list.add(new FactSet("Lightning can strike the same place twice.", "Humans share 98% of their DNA with chimpanzees.", "The Sahara Desert is the largest desert in the world.", "A", "B"));
+        return list;
+    }
 }
