2ba39858dae53c11a4eb82e05b8ac0c35c75145f
yy106-Elaine <yy106@wellesley.edu>
2025-04-22 13:18:36 -0400

    Update Owl.java

--- Owl.java
+++ Owl.java
@@ -21,30 +21,7 @@ public class Owl extends Animal {
     }
 
     public boolean playTruthGame(String answerString) {
-
         return answerString.contains(set.truths[0]) && answerString.contains(set.truths[1]) && answerString.length() == 2;
-
-        /*if (correct) {
-            System.out.println("Congratulations! You're now getting to the next room.");
-            movePlayertoRoom3();
-        } else {
-            System.out.println("Wrong answer! The Owl is not amused...");
-            System.out.println("The correct answers were: " + set.truths[0] + " and " + set.truths[1]);
-            System.out.println("You lose 10 health points.");
-            Player player = new Player();
-            player.takeDamage(10);
-            System.out.println("You have " + player.getHealth() + " health points left.");
-            if (player.getHealth() <= 0) {
-                System.out.println("Game over! You have no health left.");
-                // Reset player or end game
-                System.out.println("You are back to room0.");
-                player.resetPlayer();
-            }
-            else{
-                movePlayertoRoom2();
-            }
-            
-        }*/
     }
     
     public String correctString(){
