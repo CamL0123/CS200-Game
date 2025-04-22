4fffbe6a541d0d0618c18323e5ccbe9ff5be1c50
IsabelaSanchez <isabelanatyst26@gmail.com>
2025-04-22 12:55:34 -0400

    clean snake

--- Snake.java
+++ Snake.java
@@ -15,32 +15,26 @@ public class Snake extends Animal {
         super("Snake", 10);
     }
     
-    public String SnakeRiddle(String answer){
-    Scanner scanner = new Scanner(System.in);  
-    String answerA;
-    String answerB;
-    String answerC;
-    answerA = "king cobra"; 
-    answerB = "sand snake";
-    answerC = "poisonous snake";
-    // Riddle
-    System.out.println("Snake Challenge: Riddle! Answer the following question and you will be able to open the door to the next room");
-    System.out.println("Category is Snakes!!!");
-    System.out.println("With scales that shimmer like jewels, I am a creature of beauty and danger. What am I?");
-    System.out.println("A. " + answerA);
-    System.out.println("B. " + answerB);
-    System.out.println("C. " + answerC);
-    System.out.print("Enter your answer (e.g., A): ");
-
-    String userAnswer = scanner.nextLine().toUpperCase();
-
-    if (answer.equals("A")) {
-        return("Congratulations! You open the door to the next room.");
-        movePlayertoRoom2();
-    } else {
-        return("Wrong answer! Snake wants to bite you");
+        public String getRiddle() {
+        return "Snake Challenge: Riddle! Answer the following question and you will be able to open the door to the next room\n"+
+        "Category is Snakes!!!\n"+
+        "With scales that shimmer like jewels, I am a creature of beauty and danger. What am I?\n"+
+        "A: king cobra\n" +
+        "B: sand snake\n" +
+        "C: poisonous snake\n" +
+        "Enter your answer (e.g., A): ";
     }
-        
+    
+    public boolean playTruthGame(String answerString) {
+        return answerString.equals("A");
+    }
+    
+    public String correctString(){
+        return "Congratulations! You're now getting to the next room.";
+    }
+    
+    public String incorrectString(){
+        return "Wrong answer! Snake wants to bite you";
     }
 
 }
